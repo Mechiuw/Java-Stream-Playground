22245f93a42126c177f19b69e6aec87fb75855ed
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-06-03 23:24:56 +0700

    [FEATURE] added java-stream-oprations

--- src/main/java/playground/Playground.java
+++ src/main/java/playground/Playground.java
@@ -1,7 +1,10 @@
 package playground;
 
-import java.util.Arrays;
-import java.util.Collection;
+import java.util.*;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Playground {
@@ -10,8 +13,14 @@ public class Playground {
     }
 
     public static void lib(){
-        streamOfCollection();
-        streamOfArray();
+//        streamOfCollection();
+//        streamOfArray();
+//        streamBuilder();
+//        streamGenerate();
+//        streamIterate();
+//        streamOfPrimitives();
+//        streamOfString();
+        streamOperations();
     }
 
     public static void streamOfCollection(){
@@ -39,6 +48,89 @@ public class Playground {
     }
 
     public static void streamBuilder(){
-        
+        Stream<String> streamBuilderOne = Stream.<String>builder().add("dino").add("sau").add("rus").build();
+        Stream<String> streamBuilderTwo = Stream.<String>builder().add("jalan").add("-").add("jalan").build();
+        System.out.println("builder 1: ");
+        streamBuilderOne.forEach(System.out::print);
+        System.out.println("builder 2: ");
+        streamBuilderTwo.forEach(System.out::print);
+    }
+
+    public static void streamGenerate(){
+        Stream<String> streamGenerated = Stream.generate(() -> "element").limit(5); // this can generate or renders
+        streamGenerated.forEach(System.out::println);
+
+        Stream<Integer> streamNum = Stream.generate(() -> 10).limit(2);
+        streamNum.forEach(System.out::println);
+    }
+
+    public static void streamIterate(){
+        Stream<Integer> streamIteratedOne = Stream.iterate(1,n -> n + 1).limit(5);
+        Stream<Integer> streamIteratedTwo = Stream.iterate(1,n -> n + 2).limit(5);
+        Stream<Integer> streamIteratedThree = Stream.iterate(0,n -> n + 2).limit(5);
+        System.out.println("urut: ");
+        streamIteratedOne.forEach(System.out::print);
+        System.out.println("\nganjil: ");
+        streamIteratedTwo.forEach(System.out::print);
+        System.out.println("\ngenap: ");
+        streamIteratedThree.forEach(System.out::print);
+    }
+
+    public static void streamOfPrimitives(){
+        IntStream intStreamEx = IntStream.range(1,5); // .range(int starts exclusive, int ends exclusive)
+        IntStream intStreamIn = IntStream.rangeClosed(1,5); // .range(int starts inclusive, int ends inclusive)
+        Random random = new Random();
+        DoubleStream doubleStream = random.doubles(2);
+
+        System.out.println("stream of primitive exclusive .range() : ");
+        intStreamEx.forEach(System.out::print);
+        System.out.println("\nstream of primitive inclusive .rangeClosed() : ");
+        intStreamIn.forEach(System.out::print);
+        System.out.println("\nstream of primitive double stream .doubles() : ");
+        doubleStream.forEach(System.out::print);
+    }
+
+    public static void streamOfString(){
+        IntStream streamOfChars = "AaBbCcDd".chars(); //
+        Stream<String> streamOfString = Pattern.compile(", ").splitAsStream("a.b,c"); // split it using regex validation
+        System.out.println("Stream of chars : ");
+        streamOfChars.forEach(System.out::print);
+        System.out.println("\nStream of String :");
+        streamOfString.forEach(System.out::print);
+
+    }
+
+    public static void streamOperations(){
+        //filter() , sorted()
+        Collection<Integer> intCollection = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+        Stream<Integer> streamedInt = intCollection.stream().filter(x -> x % 2 ==0).sorted();
+        System.out.println("stream odd numbers : \n");
+        streamedInt.forEach(System.out::print);
+
+        //flatMap() strings
+        Collection<String> strCollections = Arrays.asList("ab","cd","ef","gh","ij","kl");
+        System.out.println("\none-lined streams of chars: ");
+        strCollections.stream().flatMap(x -> x.chars().mapToObj(c -> (char) c)).forEach(System.out::print);
+
+        //flatmap integers
+        Collection<Collection<Integer>> listCollections = Arrays.asList(Arrays.asList(1,2,3),Arrays.asList(4,5,6));
+        List<Integer> flattenedList = listCollections.stream().flatMap(Collection::stream).toList();
+        System.out.println("\none-lined streams of integers:");
+        flattenedList.forEach(System.out::print);
+
+        //sequential processing (sum)
+        List<Integer> numbers = Arrays.asList(1,2,3,4,5);
+        int sum = numbers.stream()
+                .reduce(0,Integer::sum);
+        System.out.println("\nsum: " + sum);
+
+        //parallel processing (sum)
+
+        int sums = numbers.parallelStream()
+                .reduce(0,Integer::sum);
+        System.out.println("parallel-sum: " + sums);
+
+        int z = numbers.stream().reduce(0,Integer::sum);
+        System.out.println("z sum : " +z);
     }
 }
