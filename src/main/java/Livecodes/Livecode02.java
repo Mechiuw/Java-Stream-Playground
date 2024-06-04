c41738c21af1e079417659b4345e982ff793b108
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-06-04 21:56:14 +0700

    [LIVECODE] Added livecode-02

---
+++ src/main/java/Livecodes/Livecode02.java
@@ -0,0 +1,63 @@
+package Livecodes;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Livecode02 {
+    public static void run() {
+        one();
+        two();
+        three();
+        four();
+        five();
+    }
+
+    public static void one(){
+        List<String> firstArr = Arrays.asList("one","two");
+        List<String> secondArr = Arrays.asList("three","four");
+        List<String> merged = Stream.concat(firstArr.stream(),secondArr.stream()).toList();
+        System.out.print("1) ");
+        merged.forEach(System.out::print);
+        System.out.println();
+    }
+
+    public static void two(){
+        String strings = "Alpha Beta Charlie Delta Echo Foxtrot Golf";
+        List<String> a = Stream.of(strings.split(" ")).toList();
+        System.out.println("2a) word counted : " + a.size());
+        System.out.print("2b) length counted : ");
+        a.stream().map(String::length).forEach(c -> System.out.print(c + "."));
+        System.out.println();
+    }
+
+    public static void three(){
+        String numbers = "632879931259";
+        List<Integer> converted = numbers.chars().mapToObj(Character::getNumericValue).toList();
+        System.out.print("3) line of numbers to prime numbers : ");
+        converted.stream().filter(Livecode02::isPrime).sorted().forEach(c -> System.out.print(c + "."));
+        System.out.println();
+    }
+
+    public static boolean isPrime(int num){
+        if(num <= 1){
+            return false;
+        }
+        return IntStream.rangeClosed(2,(int) Math.sqrt(num)).noneMatch(i -> num % i == 0);
+    }
+
+    public static void four(){
+        String numbers = "63287993125129470712731464217327939";
+        List<Integer> converted = numbers.chars().mapToObj(Character::getNumericValue).toList();
+        System.out.print("4a) inserted num : " + numbers + " \n4b) unique numbers : ");
+        converted.stream().distinct().sorted().forEach(System.out::print);
+        System.out.println();
+    }
+
+    public static void five(){
+        String strings = "Alpha Beta Charlie Delta Echo Foxtrot Golf";
+        List<String> a = Stream.of(strings.split(" ")).toList();
+        System.out.print("5) frequency of [" + strings + "] : " + a.size());
+    }
+}
--- src/main/java/entrypoint/App.java
+++ src/main/java/entrypoint/App.java
@@ -1,10 +1,12 @@
 package entrypoint;
 
 import Livecodes.Livecode01;
+import Livecodes.Livecode02;
 
 public class App {
     public static void main(String[] args) {
 //        Playground.run();
-        Livecode01.run();
+//        Livecode01.run();
+        Livecode02.run();
     }
 }
